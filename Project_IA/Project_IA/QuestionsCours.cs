143b7b5940e280b63ab366166c61e0b8396c1901
noemie-mathio <nmathio@ensc.fr>
2018-10-18 14:53:35 +0200

    jty

--- Project_IA/Project_IA/QuestionsCours.cs
+++ Project_IA/Project_IA/QuestionsCours.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Project_IA
 {
@@ -14,6 +17,7 @@ namespace Project_IA
         private string reponse3;
         private string reponse4;
         private string bonnereponse;
+        
 
         public QuestionsCours(string _question, string _reponse1, string _reponse2, string _reponse3, string _reponse4, string _bonnereponse)
         {
@@ -24,5 +28,9 @@ namespace Project_IA
             reponse4 = _reponse4;
             bonnereponse = _bonnereponse;
         }
+
+        public static List<QuestionsCours> Deserialize (string xml)
+
+
     }
 }
