c566dfbe661d0c8a523aa5df28f90c005713c43b
noemie-mathio <nmathio@ensc.fr>
2018-11-04 16:56:54 +0100

    hr

--- Project_IA/Project_IA/Test.cs
+++ Project_IA/Project_IA/Test.cs
@@ -12,56 +12,85 @@ namespace Project_IA
 {
     public partial class Test : Form
     {
+        string question;
+        string reponse1;
+        string reponse2;
+        string reponse3;
+        string reponse4;
+        int bonnerep;
+        string explication;
+        bool reponse;
+        int score;
         public Test()
         {
             InitializeComponent();
+            score = 0;
+
         }
         private void newQuestionButton_Click(object sender, EventArgs e)
         {
              List<QuestionsCours> test = QuestionsCours.quizzzzz;
             foreach (QuestionsCours element in QuestionsCours.quizzzzz)
             {
-                string question = element.question;
-                string reponse1 = element.reponse1;
-                string reponse2 = element.reponse2;
-                string reponse3 = element.reponse3;
-                string reponse4 = element.reponse4;
-                string bonnerep = element.bonnereponse;
-                string explication = element.explicationBonneReponse;
-
-
-
-
-
+                 question = element.question;
+                 reponse1 = element.reponse1;
+                 reponse2 = element.reponse2;
+                 reponse3 = element.reponse3;
+                 reponse4 = element.reponse4;
+                 bonnerep = element.bonnereponse;
+                 explication = element.explicationBonneReponse;
+                
+  
             }
-            if (answered == false)
-            {
-                return;
-            }
-            answered = false;
+           
+            option1Button.Text = reponse1;
+            option2Button.Text = reponse2;
+            option3Button.Text = reponse3;
+            option4Button.Text = reponse4;
 
-            Song answer1 = question.answer1;
-            Song answer2 = question.answer2;
-            Song answer3 = question.answer3;
-
-            option1Button.Text = answer1.artist;
-            option2Button.Text = answer2.artist;
-            option3Button.Text = answer3.artist;
-
-            answerNum = random.Next(1, 4);
-            if (answerNum == 1)
-                correctAnswer = answer1;
-            else if (answerNum == 2)
-                correctAnswer = answer2;
-            else
-                correctAnswer = answer3;
-
-            player = new Player(correctAnswer.path);
-            songTimer.Start();
+           
         }
-        private void button1_Click(object sender, EventArgs e)
+        private bool verificationQuestion(int nbDeLaReponse)
+        {
+            if (nbDeLaReponse == this.bonnerep)
+            {
+                score++;                
+                scoreLabel.Text = "Score: " + score.ToString();
+                MessageBox.Show("Bonne réponse");
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Faux");
+                return false;
+            }
+        }
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+            reponse = verificationQuestion(1);
+        }
+
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+            reponse = verificationQuestion(2);
+
+        }
+        private void option3Button_Click(object sender, EventArgs e)
+        {
+            reponse = verificationQuestion(3);
+
+        }
+        private void option4Button_Click(object sender, EventArgs e)
+        {
+            reponse = verificationQuestion(4);
+
+        }
+
+        private void Test_Load(object sender, EventArgs e)
         {
 
         }
+
+        
     }
 }
