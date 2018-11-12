3a179a7831a107eb11f571573f57ad74fc780d72
noemie-mathio <nmathio@ensc.fr>
2018-11-12 15:06:31 +0100

    Partie 1 presque finie

--- Project_IA/Project_IA/NouvelleQuestion.Designer.cs
+++ Project_IA/Project_IA/NouvelleQuestion.Designer.cs
@@ -35,6 +35,21 @@
             this.validerIdentifiantButton = new System.Windows.Forms.Button();
             this.msgErreurLabel = new System.Windows.Forms.Label();
             this.titreLabel = new System.Windows.Forms.Label();
+            this.questionlabel = new System.Windows.Forms.Label();
+            this.questiontextBox = new System.Windows.Forms.TextBox();
+            this.reponse1label = new System.Windows.Forms.Label();
+            this.reponse2label = new System.Windows.Forms.Label();
+            this.reponse3label = new System.Windows.Forms.Label();
+            this.reponse4label = new System.Windows.Forms.Label();
+            this.reponse1textBox = new System.Windows.Forms.TextBox();
+            this.reponse2textBox = new System.Windows.Forms.TextBox();
+            this.reponse3textBox = new System.Windows.Forms.TextBox();
+            this.reponse4textBox = new System.Windows.Forms.TextBox();
+            this.bonnereponselabel = new System.Windows.Forms.Label();
+            this.bonnereponsetextBox = new System.Windows.Forms.TextBox();
+            this.explicationBonneReponselabel = new System.Windows.Forms.Label();
+            this.explicationBonneReponsetextBox = new System.Windows.Forms.TextBox();
+            this.Envoyerbutton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // pseudoLabel
@@ -98,11 +113,149 @@
             this.titreLabel.TabIndex = 6;
             this.titreLabel.Text = "Ajouter une nouvelle question";
             // 
+            // questionlabel
+            // 
+            this.questionlabel.AutoSize = true;
+            this.questionlabel.Location = new System.Drawing.Point(29, 55);
+            this.questionlabel.Name = "questionlabel";
+            this.questionlabel.Size = new System.Drawing.Size(55, 13);
+            this.questionlabel.TabIndex = 7;
+            this.questionlabel.Text = "Question :";
+            // 
+            // questiontextBox
+            // 
+            this.questiontextBox.Location = new System.Drawing.Point(103, 52);
+            this.questiontextBox.Multiline = true;
+            this.questiontextBox.Name = "questiontextBox";
+            this.questiontextBox.Size = new System.Drawing.Size(261, 20);
+            this.questiontextBox.TabIndex = 8;
+            // 
+            // reponse1label
+            // 
+            this.reponse1label.AutoSize = true;
+            this.reponse1label.Location = new System.Drawing.Point(29, 91);
+            this.reponse1label.Name = "reponse1label";
+            this.reponse1label.Size = new System.Drawing.Size(74, 13);
+            this.reponse1label.TabIndex = 9;
+            this.reponse1label.Text = "Proposition 1 :";
+            // 
+            // reponse2label
+            // 
+            this.reponse2label.AutoSize = true;
+            this.reponse2label.Location = new System.Drawing.Point(29, 134);
+            this.reponse2label.Name = "reponse2label";
+            this.reponse2label.Size = new System.Drawing.Size(74, 13);
+            this.reponse2label.TabIndex = 10;
+            this.reponse2label.Text = "Proposition 2 :";
+            // 
+            // reponse3label
+            // 
+            this.reponse3label.AutoSize = true;
+            this.reponse3label.Location = new System.Drawing.Point(29, 177);
+            this.reponse3label.Name = "reponse3label";
+            this.reponse3label.Size = new System.Drawing.Size(74, 13);
+            this.reponse3label.TabIndex = 11;
+            this.reponse3label.Text = "Proposition 3 :";
+            // 
+            // reponse4label
+            // 
+            this.reponse4label.AutoSize = true;
+            this.reponse4label.Location = new System.Drawing.Point(29, 221);
+            this.reponse4label.Name = "reponse4label";
+            this.reponse4label.Size = new System.Drawing.Size(74, 13);
+            this.reponse4label.TabIndex = 12;
+            this.reponse4label.Text = "Proposition 4 :";
+            // 
+            // reponse1textBox
+            // 
+            this.reponse1textBox.Location = new System.Drawing.Point(103, 88);
+            this.reponse1textBox.Name = "reponse1textBox";
+            this.reponse1textBox.Size = new System.Drawing.Size(261, 20);
+            this.reponse1textBox.TabIndex = 13;
+            // 
+            // reponse2textBox
+            // 
+            this.reponse2textBox.Location = new System.Drawing.Point(103, 131);
+            this.reponse2textBox.Name = "reponse2textBox";
+            this.reponse2textBox.Size = new System.Drawing.Size(261, 20);
+            this.reponse2textBox.TabIndex = 14;
+            // 
+            // reponse3textBox
+            // 
+            this.reponse3textBox.Location = new System.Drawing.Point(103, 177);
+            this.reponse3textBox.Name = "reponse3textBox";
+            this.reponse3textBox.Size = new System.Drawing.Size(261, 20);
+            this.reponse3textBox.TabIndex = 15;
+            // 
+            // reponse4textBox
+            // 
+            this.reponse4textBox.Location = new System.Drawing.Point(103, 218);
+            this.reponse4textBox.Name = "reponse4textBox";
+            this.reponse4textBox.Size = new System.Drawing.Size(261, 20);
+            this.reponse4textBox.TabIndex = 16;
+            // 
+            // bonnereponselabel
+            // 
+            this.bonnereponselabel.AutoSize = true;
+            this.bonnereponselabel.Location = new System.Drawing.Point(32, 264);
+            this.bonnereponselabel.Name = "bonnereponselabel";
+            this.bonnereponselabel.Size = new System.Drawing.Size(192, 13);
+            this.bonnereponselabel.TabIndex = 17;
+            this.bonnereponselabel.Text = "Entrez le numéro de la bonne réponse :";
+            // 
+            // bonnereponsetextBox
+            // 
+            this.bonnereponsetextBox.Location = new System.Drawing.Point(230, 262);
+            this.bonnereponsetextBox.Name = "bonnereponsetextBox";
+            this.bonnereponsetextBox.Size = new System.Drawing.Size(39, 20);
+            this.bonnereponsetextBox.TabIndex = 18;
+            // 
+            // explicationBonneReponselabel
+            // 
+            this.explicationBonneReponselabel.AutoSize = true;
+            this.explicationBonneReponselabel.Location = new System.Drawing.Point(32, 295);
+            this.explicationBonneReponselabel.Name = "explicationBonneReponselabel";
+            this.explicationBonneReponselabel.Size = new System.Drawing.Size(138, 13);
+            this.explicationBonneReponselabel.TabIndex = 19;
+            this.explicationBonneReponselabel.Text = "Explication bonne réponse :";
+            // 
+            // explicationBonneReponsetextBox
+            // 
+            this.explicationBonneReponsetextBox.Location = new System.Drawing.Point(164, 295);
+            this.explicationBonneReponsetextBox.Multiline = true;
+            this.explicationBonneReponsetextBox.Name = "explicationBonneReponsetextBox";
+            this.explicationBonneReponsetextBox.Size = new System.Drawing.Size(200, 20);
+            this.explicationBonneReponsetextBox.TabIndex = 20;
+            // 
+            // button1
+            // 
+            this.Envoyerbutton.Location = new System.Drawing.Point(50, 332);
+            this.Envoyerbutton.Name = "button1";
+            this.Envoyerbutton.Size = new System.Drawing.Size(75, 23);
+            this.Envoyerbutton.TabIndex = 21;
+            this.Envoyerbutton.Text = "button1";
+            this.Envoyerbutton.UseVisualStyleBackColor = true;
+            // 
             // NouvelleQuestion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(376, 350);
+            this.ClientSize = new System.Drawing.Size(384, 367);
+            this.Controls.Add(this.Envoyerbutton);
+            this.Controls.Add(this.explicationBonneReponsetextBox);
+            this.Controls.Add(this.explicationBonneReponselabel);
+            this.Controls.Add(this.bonnereponsetextBox);
+            this.Controls.Add(this.bonnereponselabel);
+            this.Controls.Add(this.reponse4textBox);
+            this.Controls.Add(this.reponse3textBox);
+            this.Controls.Add(this.reponse2textBox);
+            this.Controls.Add(this.reponse1textBox);
+            this.Controls.Add(this.reponse4label);
+            this.Controls.Add(this.reponse3label);
+            this.Controls.Add(this.reponse2label);
+            this.Controls.Add(this.reponse1label);
+            this.Controls.Add(this.questiontextBox);
+            this.Controls.Add(this.questionlabel);
             this.Controls.Add(this.titreLabel);
             this.Controls.Add(this.msgErreurLabel);
             this.Controls.Add(this.validerIdentifiantButton);
@@ -126,5 +279,20 @@
         private System.Windows.Forms.Button validerIdentifiantButton;
         private System.Windows.Forms.Label msgErreurLabel;
         private System.Windows.Forms.Label titreLabel;
+        private System.Windows.Forms.Label questionlabel;
+        private System.Windows.Forms.TextBox questiontextBox;
+        private System.Windows.Forms.Label reponse1label;
+        private System.Windows.Forms.Label reponse2label;
+        private System.Windows.Forms.Label reponse3label;
+        private System.Windows.Forms.Label reponse4label;
+        private System.Windows.Forms.TextBox reponse1textBox;
+        private System.Windows.Forms.TextBox reponse2textBox;
+        private System.Windows.Forms.TextBox reponse3textBox;
+        private System.Windows.Forms.TextBox reponse4textBox;
+        private System.Windows.Forms.Label bonnereponselabel;
+        private System.Windows.Forms.TextBox bonnereponsetextBox;
+        private System.Windows.Forms.Label explicationBonneReponselabel;
+        private System.Windows.Forms.TextBox explicationBonneReponsetextBox;
+        private System.Windows.Forms.Button Envoyerbutton;
     }
 }
